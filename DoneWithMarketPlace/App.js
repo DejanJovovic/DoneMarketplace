c2b8a5ccbcae6258ca003cd8f21340f51546cadc
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2024-07-19 16:27:29 +0200

    listings screen displays the products for sale

--- DoneWithMarketPlace/App.js
+++ DoneWithMarketPlace/App.js
@@ -10,7 +10,8 @@ import Icon from "./app/components/Icon";
 import Screen from "./app/components/screen";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <AccountScreen></AccountScreen>;
+  return <ListingsScreen></ListingsScreen>;
 }
