39fd73c3f29723c08b89f1a9ca1893a7b707b5e1
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2024-07-19 23:31:21 +0200

    built the login screen with Formik and validating the data with Yup

---
+++ DoneWithMarketPlace/app/screens/LoginScreen.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Image, StyleSheet } from "react-native";
+import Screen from "../components/screen";
+import AppTextInput from "../components/AppTextInput";
+import AppButton from "../components/AppButton";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import AppText from "../components/AppText";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
+
+function LoginScreen(props) {
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/logo.png")} />
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        {({ handleChange, handleSubmit, errors }) => (
+          <>
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="email"
+              keyboardType="email-address"
+              onChange={handleChange("email")}
+              placeholder="Email"
+            />
+            <AppText style={{ color: "red" }}>{errors.email}</AppText>
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="lock"
+              onChangeText={handleChange("password")}
+              secureTextEntry
+              placeholder="Password"
+            />
+            <AppText style={{ color: "red" }}>{errors.password}</AppText>
+
+            <AppButton title="Login" onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
+
+export default LoginScreen;
